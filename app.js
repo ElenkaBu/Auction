9463b2cf27a1f4168fd5d2e193042b7ff9df8b8c
Elena <bubeevalena@gmail.com>
2022-12-14 19:40:35 +0300

    install react and babel

--- app.js
+++ app.js
@@ -3,11 +3,12 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT ?? 3000;
 
+require('@babel/register');
 const morgan = require('morgan');
 require('dotenv').config();
 
 app.use(morgan('env'));
 
 app.listen(PORT, () => {
-  console.log(`Server started at ${PORT} port`)
+  console.log(`Server started at ${PORT} port`);
 });
